b3ececee4afcde5604d21ad2128beb178ec91277
Alessandro Galli <a.galli85@gmail.com>
2017-01-25 23:22:31 +0100

    deploy script improvements

--- deploy.php
+++ deploy.php
@@ -1,44 +1,47 @@
 <?php
-/*
- * This file has been generated automatically.
- * Please change the configuration for correct use deploy.
- */
+
+namespace Deployer;
+
+use function Deployer\{server, task, run, set, get, add, before, after};
 
 require 'recipe/symfony3.php';
 
+/** @var array $_dep */
+require './deploy_vars.php';
+
 // Set configurations
-set('repository', 'https://github.com/Algatux/wallet-project.git');
+set('repository', $_dep['repository']);
 set('shared_files', []);
 set('shared_dirs', ['var/logs', 'var/sessions', 'var/storage']);
 set('writable_dirs', ['var/cache', 'var/logs', 'var/sessions', 'var/storage']);
 
 // Configure servers
-server('production', '104.236.15.230')
-    ->env('deploy_path', '/var/www/wallet-backend')
-    ->user('root')
-    ->identityFile('~/.ssh/id_rsa.pub', '~/.ssh/id_rsa')
+server('production', $_dep['server']['address'], $_dep['server']['port'])
+    ->set('deploy_path', $_dep['server']['deploy_path'])
+    ->user($_dep['server']['user'])
+    ->identityFile($_dep['server']['key_pub'], $_dep['server']['key_priv'], $_dep['server']['key_secret'])
 ;
 
 /**
  * Copy production parameters yml
  */
-task('config:copy', function () {
-    run('cp /var/www/shared/config/parameters.yml.wallet-backend /var/www/wallet-backend/release/app/config/parameters.yml');
-    run('cp /var/www/shared/config/sentry.yml.wallet-backend /var/www/wallet-backend/release/app/config/vendors/sentry.yml');
+task('config:copy', function () use ($_dep) {
+    run('cp /var/www/shared/config/parameters.yml.wallet-backend '.$_dep['server']['deploy_path'].'/release/app/config/parameters.yml');
+    run('cp /var/www/shared/config/sentry.yml.wallet-backend '.$_dep['server']['deploy_path'].'/release/app/config/vendors/sentry.yml');
 })->desc('Copies parameter yml');
 /**
  * fix permissions
  */
-task('permissions:fix', function () {
-    run('sudo chown -R www-data:www-data /var/www/wallet-backend/release/*');
-    run('sudo chown -R www-data:www-data /var/www/wallet-backend/shared/var/logs');
-    run('sudo chown -R www-data:www-data /var/www/wallet-backend/shared/var/sessions');
+task('permissions:fix', function () use ($_dep)  {
+    run('sudo chown -R www-data:www-data '.$_dep['server']['deploy_path'].'/release/*');
+    run('sudo chown -R www-data:www-data '.$_dep['server']['deploy_path'].'/shared/var/logs');
+    run('sudo chown -R www-data:www-data '.$_dep['server']['deploy_path'].'/shared/var/sessions');
 })->desc('Fixes permissions');
 /**
  * Assets dump!
  */
-task('assets:dump', function () {
-    run('/var/www/wallet-backend/release/bin/console assetic:dump --env=prod');
+task('assets:dump', function () use ($_dep)  {
+    run($_dep['server']['deploy_path'].'/release/bin/console assetic:dump --env=prod');
 })->desc('Assets dump!');
 
 before('deploy:vendors', 'config:copy');
