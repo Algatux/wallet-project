931e9214d0afb3040bbd83cdb5c41aae7e558bfb
Alessandro Galli <alessandro.galli@facile.it>
2017-07-27 10:14:58 +0200

    some fixes added oh-my-zsh

--- docker/php/shared/zsh/install-zsh-config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [ -f /tmp/zsh/zsh-config.sh ]; then
-    cp /tmp/zsh/zsh-config.sh /var/www/php-user/.zshrc
-else
-    cp /tmp/zsh/zsh-config.sample.sh /var/www/php-user/.zshrc
-fi
--- setup.sh
+++ setup.sh
@@ -2,5 +2,6 @@
 
 docker-compose up -d --build --force-recreate
 docker exec -ti dev_php composer install
+docker exec -ti dev_php bower install
 docker exec -ti dev_php bin/console doctrine:database:drop -n --if-exists --force
 docker-compose stop
