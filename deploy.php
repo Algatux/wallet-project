7239b35595d6f0f748df3cb45eff4de5ef3dd1c5
Alessandro Galli <a.galli85@gmail.com>
2017-11-05 18:13:11 +0100

    new deploy scripts

--- deploy.php
+++ deploy.php
@@ -2,8 +2,6 @@
 
 namespace Deployer;
 
-use function Deployer\{server, task, run, set, get, add, before, after};
-
 require 'recipe/symfony3.php';
 
 /** @var array $_dep */
@@ -18,8 +16,10 @@ set('shared_dirs', ['var/logs', 'var/sessions', 'var/storage']);
 set('writable_dirs', ['var/cache', 'var/logs', 'var/sessions', 'var/storage']);
 
 // Configure servers
-server('production', $_dep['server']['address'], $_dep['server']['port'])
+host('production')
+    ->hostname($_dep['server']['address'])
     ->set('deploy_path', $_dep['server']['deploy_path'])
+    ->port($_dep['server']['port'])
     ->user($_dep['server']['user'])
     ->identityFile($_dep['server']['key_pub'], $_dep['server']['key_priv'], $_dep['server']['key_secret'])
 ;
@@ -31,32 +31,24 @@ task('config:copy', function () use ($_dep) {
     run('cp '.$_dep['server']['shared_path'].'/parameters.yml.wallet-backend '.$_dep['server']['deploy_path'].'/release/app/config/parameters.yml');
     run('cp '.$_dep['server']['shared_path'].'/sentry.yml.wallet-backend '.$_dep['server']['deploy_path'].'/release/app/config/vendors/sentry.yml');
 })->desc('Copies parameter yml');
-/**
- * fix permissions
- */
-task('permissions:fix', function () use ($_dep)  {
-    run('chown -R www-data:www-data '.$_dep['server']['deploy_path'].'/release/*');
-    run('sudo chown -R www-data:www-data '.$_dep['server']['deploy_path'].'/shared/var/logs');
-    run('sudo chown -R www-data:www-data '.$_dep['server']['deploy_path'].'/shared/var/sessions');
-})->desc('Fixes permissions');
 
 /** bower install! */
 task('bower:install', function () use ($_dep)  {
-    run('cd {{release_path}} && bower install --allow-root');
+    run('cd {{release_path}} && bower install');
 })->desc('Install bower dependencies!');
 
 /** Assets dump! */
 task('assets:dump', function () use ($_dep)  {
-    run('{{env_vars}} {{bin/php}} {{bin/console}} assetic:dump {{console_options}}');
+    run('{{bin/php}} {{bin/console}} assetic:dump {{console_options}}');
 })->desc('Assets dump!');
 
 /** cache clear! */
 task('clear:cache', function () use ($_dep)  {
-    run('{{env_vars}} {{bin/php}} {{bin/console}} cache:clear {{console_options}}');
+    run('{{bin/php}} {{bin/console}} cache:clear {{console_options}}');
 })->desc('Cleares symfony cache!');
 
 before('deploy:vendors', 'config:copy');
-after('config:copy', 'permissions:fix');
+//after('config:copy', 'permissions:fix');
 after('deploy:vendors', 'assets:dump');
 before('deploy:cache:warmup', 'clear:cache');
 before('assets:dump', 'bower:install');
