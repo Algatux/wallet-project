d5a7eaee5c5c873f0a37df8a3be230e0db907fe2
Alessandro Galli <a.galli85@gmail.com>
2017-11-08 23:27:06 +0100

    - dep script override default

--- deploy.php
+++ deploy.php
@@ -25,33 +25,65 @@ host('production')
     ->identityFile($_dep['server']['key_pub'], $_dep['server']['key_priv'], $_dep['server']['key_secret']);
 
 /** Copy production parameters yml */
-task('config:copy', function () use ($_dep) {
+task('deploy:config:copy', function () use ($_dep) {
     run('cp '.$_dep['server']['shared_path'].'/parameters.yml.wallet-backend '.$_dep['server']['deploy_path'].'/release/app/config/parameters.yml');
     run('cp '.$_dep['server']['shared_path'].'/sentry.yml.wallet-backend '.$_dep['server']['deploy_path'].'/release/app/config/vendors/sentry.yml');
 })->desc('Copies parameter yml');
 
 /** bower install! */
-task('bower:install', function () use ($_dep)  {
+task('deploy:bower:install', function () use ($_dep)  {
     run('cd {{release_path}} && bower install');
 })->desc('Install bower dependencies!');
 
 /** Assets dump! */
-task('assets:dump', function () use ($_dep)  {
+task('deploy:assetic:dump', function () use ($_dep)  {
     run('{{bin/php}} {{bin/console}} assetic:dump {{console_options}}');
 })->desc('Assets dump!');
 
 /** cache clear! */
-task('clear:cache', function () use ($_dep)  {
+task('deploy:cache:clear', function () use ($_dep)  {
     run('{{bin/php}} {{bin/console}} cache:clear {{console_options}}');
 })->desc('Cleares symfony cache!');
 
+/** cache warmup! */
+task('deploy:cache:warmup', function () use ($_dep)  {
+    run('{{bin/php}} {{bin/console}} cache:warmup {{console_options}}');
+})->desc('Warmup symfony cache!');
+
 /** db migrations! */
-task('db:migrations', function () use ($_dep)  {
+task('database:migrate', function () use ($_dep)  {
     run('{{bin/php}} {{bin/console}} doctrine:migrations:migrate {{console_options}}');
 })->desc('Applies database migrations!');
 
-before('deploy:vendors', 'config:copy');
-after('deploy:vendors', 'assets:dump');
-before('deploy:cache:warmup', 'clear:cache');
-before('assets:dump', 'bower:install');
-after('deploy:assetic:dump', 'db:migrations');
+/**
+ * Main task
+ */
+task('deploy', [
+    'deploy:info',
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:clear_paths',
+    'deploy:create_cache_dir',
+    'deploy:shared',
+    'deploy:config:copy',
+    'deploy:assets',
+    'deploy:vendors',
+    'database:migrate',
+    'deploy:assets:install',
+    'deploy:bower:install',
+    'deploy:assetic:dump',
+    'deploy:cache:clear',
+    'deploy:writable',
+    'deploy:cache:warmup',
+    'deploy:symlink',
+    'deploy:unlock',
+    'cleanup',
+])->desc('Deploy your project');
+
+//before('deploy:vendors', 'config:copy');
+//after('deploy:vendors', 'assets:dump');
+//before('deploy:cache:warmup', 'clear:cache');
+//before('assets:dump', 'bower:install');
+//after('deploy:assetic:dump', 'db:migrations');
