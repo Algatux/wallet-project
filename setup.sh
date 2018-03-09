1122c7d7489cdd959c3b953805a37eca0a72a95d
Alessandro Galli <alessandro.galli@facile.it>
2018-03-09 16:31:13 +0100

    wip docker deployment

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker-compose up -d --build --force-recreate
+docker-compose up -d --force-recreate
 docker exec -ti dev_php composer install
 docker exec -ti dev_php bower install
 docker exec -ti dev_php bin/console doctrine:database:drop -n --if-exists --force
