ebd40eb10c6eba61f24131d8a1b47a602a4459de
Alessandro Galli <a.galli85@gmail.com>
2018-04-28 20:57:47 +0200

    prod consumer command

---
+++ bin/prod/consumer_start.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+BROKER_CMD="bin/console amqp:message:broker -v"
+CONSUMER_CONF="app/config/vendors/amqp/consumer-prod.conf"
+
+bin/rabbitmq-cli-consumer -i -o -V -e "${BROKER_CMD}" -c "${CONSUMER_CONF}"
