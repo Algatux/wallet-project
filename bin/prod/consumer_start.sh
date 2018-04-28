f482a42564865b4a7ab71262ff6ac3e63b5d1e3f
Alessandro Galli <a.galli85@gmail.com>
2018-04-28 21:30:30 +0200

    fix command env

--- bin/prod/consumer_start.sh
+++ bin/prod/consumer_start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-BROKER_CMD="bin/console amqp:message:broker -v"
+BROKER_CMD="bin/console amqp:message:broker -v --env=prod"
 CONSUMER_CONF="app/config/vendors/amqp/consumer-prod.conf"
 
 bin/rabbitmq-cli-consumer -i -o -V -e "${BROKER_CMD}" -c "${CONSUMER_CONF}"
